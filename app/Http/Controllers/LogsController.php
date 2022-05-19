f12a3503ff029bee71f454043511273127b2e146
iwai <issin.doku2442@gmail.com>
2022-05-19 11:51:52 +0900

    業務日誌検索のコメント、コード順修正

--- app/Http/Controllers/LogsController.php
+++ app/Http/Controllers/LogsController.php
@@ -218,11 +218,11 @@ class LogsController extends Controller
         // 入力された検索キーワードを取得
         $keyword = $request->input('keyword');
 
-        // 検索(ID, 職員、で検索可能にする)
+        // 検索(ID, 日付、天気、職員で検索可能にする)
         $logs = Log::where('id','like', '%' . $keyword . '%')
-                    ->orWhere('staff', 'like', '%' . $keyword . '%')
-                    ->orWhere('weather', 'like', '%' . $keyword . '%')
                     ->orWhere('date', 'like', '%' . $keyword . '%')
+                    ->orWhere('weather', 'like', '%' . $keyword . '%')
+                    ->orWhere('staff', 'like', '%' . $keyword . '%')
                     ->paginate(10);
        
         // キーワードがなければフラッシュメッセージをnull
