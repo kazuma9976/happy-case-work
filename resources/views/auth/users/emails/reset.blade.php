d06cc943506c4ad3b3dab866cac7ded9cae418ca
Kazuma Iwai <samuraikazuma1@gmail.com>
2022-06-23 13:06:50 +0900

    修正

--- resources/views/auth/users/emails/reset.blade.php
+++ resources/views/auth/users/emails/reset.blade.php
@@ -6,6 +6,6 @@
     </div>
     <div class="row mt-3 text-center">
     　　<p>以下をクリックし、パスワードの再設定を行って下さい。</p>
-        <p class="mt-3 offset-sm-3 col-sm-6 btn btn-success"><a href="{{ $restUrl }}"></a></p>
+        <p class="mt-3 offset-sm-3 col-sm-6 btn btn-success"><a href="{{ $restUrl }}">{{ $restUrl }}</a></p>
     </div>
 @endsection
