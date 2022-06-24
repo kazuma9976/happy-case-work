d6fe1b1719af68334c26c0b085422eac685ac333
Kazuma Iwai <samuraikazuma1@gmail.com>
2022-06-24 11:24:56 +0900

    引き続きコードを修正

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,14 +1,12 @@
 @extends('layouts.app')
 @section('title', '職員一覧')
 @section('content')
-    <div class="text-center text-success mt-5 pt-4">
+    <div class="text-center text-success mt-5">
         <h1>職員一覧</h1>
     </div>
     @if($users->total() !== 0)
-    <div class="row mt-4">
-        <p class="text-danger mt-4">※登録職員 : {{ $users->total() }}人</p>
-    </div>
-    <table class="table table-bordered table-striped text-center">
+    <p class="text-danger mt-4">※登録職員 : {{ $users->total() }}人</p>
+    <table class="table table-bordered table-striped text-center" id="users">
         <tr>
             <th>ID</th>
             <th>名前</th>
@@ -18,7 +16,7 @@
         </tr>
         @foreach($users as $user)
         <tr>
-            <td>{!! link_to_route('users.show', $user->id , ['id' => $user->id], ['class' => "btn btn-success"]) !!}</td>
+            <td>{!! link_to_route('users.show', $user->id , ['id' => $user->id], ['class' => 'btn btn-success']) !!}</td>
             <td>{{ $user->name }}</td>
             <td>
                 @if($user->profile)
@@ -39,7 +37,7 @@
     {{ $users->links('pagination::bootstrap-4') }}
     @else
     <div class="row mt-5">
-        <h2 class="col-sm-12 text-center text-danger mt-5">※登録職員はいません</h2>
+        <h2 class="text-center text-danger mt-5">※登録職員はいません</h2>
     </div>
     @endif
 @endsection
