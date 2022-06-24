32f23736da5b7d5399b09bdfe8f50678e58a00d1
Kazuma Iwai <samuraikazuma1@gmail.com>
2022-06-24 09:44:20 +0900

    職員一覧のコード修正、ログイン・ログアウト関連、職員登録関連にフラッシュメッセージを追加

--- resources/views/auth/users/emails/reset.blade.php
+++ resources/views/auth/users/emails/reset.blade.php
@@ -1,11 +1,9 @@
 @extends('layouts.app')
 @section('title', 'パスワード再設定のリクエスト')
 @section('content')
-    <div class="text-center">
-        <h1>パスワード再設定のリクエスト</h1>
-    </div>
-    <div class="row mt-3 text-center">
-    　　<p>以下をクリックし、パスワードの再設定を行って下さい。</p>
-        <p class="mt-3 offset-sm-3 col-sm-6 btn btn-success"><a href="{{ $restUrl }}">{{ $restUrl }}</a></p>
+    <h1>パスワード再設定のリクエスト</h1>
+    <div class="row mt-3">
+    　　<p>以下のURLをクリックし、パスワードの再設定を行って下さい。</p>
+        <p class="mt-3"><a href="{{ $restUrl }}">{{ $restUrl }}</a></p>
     </div>
 @endsection
--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -3,42 +3,40 @@
 @section('content')
     <div class="text-center text-success mt-5 pt-4">
         <h1>職員一覧</h1>
-        @if($users->total() !== 0)
-        <div class="row mt-4">
-            <p class="text-danger mt-4">※登録職員 : {{ $users->total() }}人</p>
-        </div>
-        <div class="row">
-            <table class="table table-bordered table-striped text-center">
-                <tr>
-                    <th>ID</th>
-                    <th>名前</th>
-                    <th>プロフィール画像</th>
-                    <th>メールアドレス</th>
-                    <th>登録日時</th>
-                </tr>
-                @foreach($users as $user)
-                <tr>
-                    <td>{!! link_to_route('users.show', $user->id , ['id' => $user->id], ['class' => "btn btn-success"]) !!}</td>
-                    <td>{{ $user->name }}</td>
-                    <td>
-                        @if($user->profile)
-                            @if($user->profile->image)
-                            <img src="{{ Storage::disk('s3')->url('uploads/' . $user->profile->image) }}" alt="{{ $user->profile->image }}" id="image_icon_list">
-                            @else
-                            <img src="{{ asset('images/no_image_human.jpg') }}" alt="画像資料はありません" id="image_icon_list">
-                            @endif
-                        @else
-                            <img src="{{ asset('images/no_image_human.jpg') }}" alt="画像資料はありません" id="image_icon_list">
-                        @endif
-                    </td>
-                    <td>{{ $user->email }}</td>
-                    <td class="text-primary">{{ $user->created_at }}</td>
-                </tr>
-                @endforeach
-            </table>
-            {{ $users->links('pagination::bootstrap-4') }}
-        </div>
     </div>
+    @if($users->total() !== 0)
+    <div class="row mt-4">
+        <p class="text-danger mt-4">※登録職員 : {{ $users->total() }}人</p>
+    </div>
+    <table class="table table-bordered table-striped text-center">
+        <tr>
+            <th>ID</th>
+            <th>名前</th>
+            <th>プロフィール画像</th>
+            <th>メールアドレス</th>
+            <th>登録日時</th>
+        </tr>
+        @foreach($users as $user)
+        <tr>
+            <td>{!! link_to_route('users.show', $user->id , ['id' => $user->id], ['class' => "btn btn-success"]) !!}</td>
+            <td>{{ $user->name }}</td>
+            <td>
+                @if($user->profile)
+                    @if($user->profile->image)
+                    <img src="{{ Storage::disk('s3')->url('uploads/' . $user->profile->image) }}" alt="{{ $user->profile->image }}" id="image_icon_list">
+                    @else
+                    <img src="{{ asset('images/no_image_human.jpg') }}" alt="画像資料はありません" id="image_icon_list">
+                    @endif
+                @else
+                    <img src="{{ asset('images/no_image_human.jpg') }}" alt="画像資料はありません" id="image_icon_list">
+                @endif
+            </td>
+            <td>{{ $user->email }}</td>
+            <td class="text-primary">{{ $user->created_at }}</td>
+        </tr>
+        @endforeach
+    </table>
+    {{ $users->links('pagination::bootstrap-4') }}
     @else
     <div class="row mt-5">
         <h2 class="col-sm-12 text-center text-danger mt-5">※登録職員はいません</h2>
