97dbc6bb43c2d9d506320fe926b8686076e3a573
kazuma iwai <issin.doku2442@gmail.com>
2022-07-13 09:04:38 +0900

    EC2自動デプロイ再度テスト

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,8 +2,8 @@
 @section('title', '相談記録 精神障害者向け')
 @section('content')
     <div class="row">
-        <h1 class="col-sm-12 text-center text-info mb-2">相談支援記録</h1>
-        <h2 class="col-sm-12 text-center text-info mt-2">～ 精神障害者向け ～</h2>
+        <h1 class="col-sm-12 text-center text-primary mb-2">相談支援記録</h1>
+        <h2 class="col-sm-12 text-center text-primary mt-2">～ 精神障害者向け ～</h2>
     </div>
     <div class="mt-4" id="mainVisual">
         <img src="{{ ('/images/network1.jpg') }}" alt="最初に表示されるtop画面">
